1480a7805ed48c7d09748a90377d17964b6fb2e5
Justin Kimbrell <jkimbrell@actengage.com>
2020-08-29 12:22:55 -0600

    added based theme

---
+++ utils/index.js
@@ -0,0 +1,21 @@
+const breakpoints = require('./breakpoints');
+const colorLevel = require('./colorLevel');
+const contrast = require('./contrast');
+const darken = require('./darken');
+const escapeSvg = require('./escapeSvg');
+const flatten = require('./flatten');
+const mix = require('./mix');
+const quote = require('./quote');
+const rgba = require('./rgba');
+
+module.exports = {
+    breakpoints,
+    colorLevel,
+    contrast,
+    darken,
+    escapeSvg,
+    flatten,
+    mix,
+    quote,
+    rgba
+};
