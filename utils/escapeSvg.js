4c29087c933edb6a77a65fc8632487f19a23b9b9
Justin Kimbrell <jkimbrell@actengage.com>
2020-09-26 08:14:48 -0600

    Fixed escape svg bugs

--- utils/escapeSvg.js
+++ utils/escapeSvg.js
@@ -1,17 +1,17 @@
 const escapedCharacters = {
     '<': '%3c',
     '>': '%3e',
-    '#': '%23',
-    '(': '%28',
-    ')': '%29',
+    '\\#': '%23',
+    '\\(': '%28',
+    '\\)': '%29',
 };
 
 module.exports = function(str) {
-    if(str.match('data:image/svg+xml')) {
-        Object.entries(escapedCharacters)
-            .reduce((carry, [char, encoded]) => {
-                if(carry.match(/url\(/)) {
-                    return `url(${str.slice(6, -3).replace(new RegExp(char, 'g'), encoded)})`;
+    if(str.match(/data\:image\/svg\+xml/)) {
+        return Object.entries(escapedCharacters)
+            .reduce((str, [char, encoded]) => {
+                if(str.match(/url\(/)) {
+                    return `url(${str.slice(4, -1).replace(new RegExp(char, 'g'), encoded)})`;
                 }
 
                 return str.replace(new RegExp(char, 'g'), encoded);
