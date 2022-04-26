46b0bc4b432b5fe1b81271d81c538d30852125c6
Justin Kimbrell <jkimbrell@actengage.com>
2022-04-26 16:10:49 -0600

    Updated escapeSvg function

--- utils/escapeSvg.js
+++ utils/escapeSvg.js
@@ -1,22 +1,48 @@
-const escapedCharacters = {
-    '<': '%3c',
-    '>': '%3e',
-    '\\#': '%23',
-    '\\(': '%28',
-    '\\)': '%29',
-};
+function encode(code) {
+    return code
+        // .replace(/%/g, "%25")
+        .replace(/\</g, '%3C')
+        .replace(/\>/g, '%3E')
+        .replace(/</g, "<")
+        .replace(/>/g, ">")
+        .replace(/&/g, "%26")
+        .replace(/#/g, "%23")
+        .replace(/{/g, "%7B")
+        .replace(/}/g, "%7D")
+        .replace(/\(/g, "%28")
+        .replace(/\)/g, "%29");
+}
+  
+function addXmlns(code) {
+    if(code.indexOf("xmlns") === -1) {
+        return code.replace(/\<svg/g, '<svg xmlns="http://www.w3.org/2000/svg"');
+    }
+    else {
+        return code;
+    }
+}
+  
+function normalize(code) {
+    return code
+        // .replace(/'/g, '%22')
+        .replace(/"/g, "'")
+        .replace(/\s+/g, ' ')
+        .replace(/\\n/g, '')
+        .trim();
+}
+  
+function transform(code) {
+    return `data:image/svg+xml;charset=utf-8,${addXmlns(code)}`;
+}
 
-module.exports = function(str) {
-    if(str.match(/data\:image\/svg\+xml/)) {
-        return Object.entries(escapedCharacters)
-            .reduce((str, [char, encoded]) => {
-                if(str.match(/url\(/)) {
-                    return `url(${str.slice(4, -1).replace(new RegExp(char, 'g'), encoded)})`;
-                }
+module.exports = function escapeSvg(str) {
+    if(!str.match(/^\<svg/)) {
+        return str;
+    }
 
-                return str.replace(new RegExp(char, 'g'), encoded);
-            }, str);
+    if(!str.match(/^data\:image\/svg\+xml/)) {
+        str = transform(encode(normalize(str)));
     }
     
-    return str;
+    return `url("${str}")`;
 };
